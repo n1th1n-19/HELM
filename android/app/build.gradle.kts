8ceeb0395286ab79e9b9455c6320e17a0a6186a5
n1th1n-19 <n1th1n-19@proton.me>
2026-06-02 21:02:36 +0530

    chore(release): bump version to 0.3.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.helm.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     buildTypes {
